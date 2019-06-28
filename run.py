a3f9f58c52c977294a09d90b471729d5c60aa5aa
keyaa <keya5541@colorado.edu>
2019-06-27 19:51:12 -0600

    added kicking, default value changes, permission checking, get_user functionality

--- run.py
+++ run.py
@@ -4,10 +4,31 @@ import profanity_check as pc
 import discord
 import asyncio
 
-bot = commands.Bot(command_prefix=".")
+mute_role = "muted"
+mod_role = "mod"
+custom_prefix = "."
+default_threshold = 0.8
+
+bot = commands.Bot(command_prefix=custom_prefix)
 blacklist = {}
 quiet_mode = False
-threshold_value = 0.8
+threshold_value = default_threshold
+
+
+def is_mod(invoker):
+  return discord.utils.get(invoker.guild.roles, name=mod_role) in invoker.roles
+
+
+async def get_user(ctx, uid, usage_str):
+  user = discord.utils.get(ctx.guild.members, id=int(uid.strip("<>@"))) # try to find user id
+  if user == None:
+    await ctx.send("User not found!")
+    await ctx.send(usage_str)
+  elif user == bot.user:
+    await ctx.send("Who, me?")
+    await ctx.send(usage_str)
+  else:
+    return user
 
 
 def is_profane(message):
@@ -18,6 +39,12 @@ def is_profane(message):
   if pc.predict([message])[0] > threshold_value: # after passing through fixed filter, use trained SVM model
     return True
   return False
+
+
+# another form of "kick" command
+@bot.command(pass_context=True)
+async def k(ctx):
+  await kick.invoke(ctx)
 
 
 # another form of "reset" command
@@ -56,19 +83,52 @@ async def m(ctx):
   await mute.invoke(ctx)
 
 
+# kick specified user
+@bot.command()
+async def kick(ctx, *args):
+  if not is_mod(ctx.author):
+    if not quiet_mode:
+      await ctx.send("You don't have permission to do that!")
+    return
+
+  def reaction_check(reaction, moderator): # needed to check if moderator reacted to messages by user
+    return moderator == ctx.message.author and str(reaction.emoji) == "✅"
+
+  user = await get_user(ctx, args[0], "Usage: '.k[ick] @user [reason]")
+  if user != None:
+    message = await ctx.send("Are you sure you want to kick ? (10 second timeout)")
+    await message.add_reaction("✅") # wait for response
+    try:
+      reaction, moderator = await bot.wait_for("reaction_add", timeout=10.0, check=reaction_check) # 10 second timeout, wait for moderator to react
+    except asyncio.TimeoutError:
+      if not quiet_mode:
+        await ctx.send("10 seconds elapsed, timed out.")
+    else:
+      if len(args) == 2:
+        await ctx.guild.kick(user, reason=args[1]) # reason provided
+      else:
+        await ctx.guild.kick(user) # no reason provided
+    await message.delete() # delete message reacted to
+
+
 # reset blacklist to default
 @bot.command()
 async def reset(ctx):
-  def check(reaction, moderator): # needed to check if moderator reacted to messages by user
+  if not is_mod(ctx.author):
+    if not quiet_mode:
+      await ctx.send("You don't have permission to do that!")
+    return
+
+  def reaction_check(reaction, moderator): # needed to check if moderator reacted to messages by user
     return moderator == ctx.message.author and str(reaction.emoji) == "✅"
 
-  message = await ctx.send("Are you sure you want to reset the blacklist? (5 second timeout)")
+  message = await ctx.send("Are you sure you want to reset the blacklist? (10 second timeout)")
   await message.add_reaction("✅") # wait for response
   try:
-    reaction, moderator = await bot.wait_for("reaction_add", timeout=5.0, check=check) # 5 second timeout, wait for moderator to react
+    reaction, moderator = await bot.wait_for("reaction_add", timeout=10.0, check=reaction_check) # 10 second timeout, wait for moderator to react
   except asyncio.TimeoutError:
     if not quiet_mode:
-      await ctx.send("5 seconds elapsed, timed out.")
+      await ctx.send("10 seconds elapsed, timed out.")
   else:
     blacklist.clear()
     copyfile("default_blacklist.txt", "blacklist.txt")
@@ -86,13 +146,18 @@ async def threshold(ctx, new_t):
   global quiet_mode
   global threshold_value
 
+  if not is_mod(ctx.author):
+    if not quiet_mode:
+      await ctx.send("You don't have permission to do that!")
+    return
+
   async def usage():
     await ctx.send("Usage:\n'.t[hreshold] (0.0 - 1.0)'")
 
   try: 
     threshold_value = float(new_t)
-    if threshold_value < 0.0 or threshold_value > 1.0:
-      threshold_value = .8
+    if threshold_value < 0.0 or threshold_value > 1.0: # outside of valid probability range
+      threshold_value = default_threshold # reset to default
       await usage()
       return
     if not quiet_mode:
@@ -117,6 +182,12 @@ async def quiet(ctx):
 @bot.command()
 async def addword(ctx, word):
   global quiet_mode
+
+  if not is_mod(ctx.author):
+    if not quiet_mode:
+      await ctx.send("You don't have permission to do that!")
+    return
+
   prev = quiet_mode
   quiet_mode = True # temporarily suppress filter reply message
   if word in blacklist:
@@ -136,25 +207,20 @@ async def addword(ctx, word):
 async def delete(ctx, *args):
   global quiet_mode
 
-  async def usage():
-    await ctx.send("Usage:\n'.d[elete] @user [# of messages to delete]")
+  if not is_mod(ctx.author):
+    if not quiet_mode:
+      await ctx.send("You don't have permission to do that!")
+    return
 
-  def check(reaction, moderator): # needed to check if moderator reacted to messages by user
+  def reaction_check(reaction, moderator): # needed to check if moderator reacted to messages by user
     return moderator == ctx.message.author and str(reaction.emoji) == "⛔"
 
   if len(args) != 1 and len(args) != 2:
-    await usage()
+    await ctx.send("Usage:\n'.d[elete] @user [# of messages to delete]")
     return
 
-  mention_id = args[0]
-  user = discord.utils.get(ctx.guild.members, id=int(mention_id.strip("<>@"))) # try to find user id
-  if user == None:
-    await ctx.send("User not found!")
-    await usage()
-  elif user == bot.user:
-    await ctx.send("Who, me?")
-    await usage()
-  else:
+  user = await get_user(ctx, args[0], "Usage:\n'.d[elete] @user [# of messages to delete]")
+  if user != None:
     msg_count = -1
     numeric = False
     if len(args) == 2: # if numberic argument provided, delete (args[1]) previous # of messages from user
@@ -164,7 +230,7 @@ async def delete(ctx, *args):
         if not quiet_mode:
           await ctx.send(user.mention + ", your previous " + str(msg_count) + " messages are inappropriate!")
       except ValueError:
-        await usage()
+        await ctx.send("Usage:\n'.d[elete] @user [# of messages to delete]")
         return
     else:
       if not quiet_mode:
@@ -186,11 +252,11 @@ async def delete(ctx, *args):
       return
 
     try:
-      reaction, moderator = await bot.wait_for("reaction_add", timeout=30.0, check=check) # 30 second timeout, wait for moderator to react
+      reaction, moderator = await bot.wait_for("reaction_add", timeout=20.0, check=reaction_check) # 20 second timeout, wait for moderator to react
       bad_message = reaction.message
       bad_message_id = bad_message.id
     except asyncio.TimeoutError:
-      await ctx.send("30 seconds elapsed, timed out.")
+      await ctx.send("20 seconds elapsed, timed out.")
     else:
       await bad_message.delete() # delete message reacted to
       if not quiet_mode:
@@ -205,28 +271,32 @@ async def delete(ctx, *args):
 # requires a "muted" role, adds role to mute users
 @bot.command()
 async def mute(ctx, *args):
-  async def usage():
-    await ctx.send("Usage: '.m[ute] @user [reason]")
-
-  mention_id = args[0]
-  print(mention_id)
-  user = discord.utils.get(ctx.guild.members, id=int(mention_id.strip("<>@"))) # try to find user id
-  print(user)
-  if user == None:
-    await ctx.send("User not found!")
-    await usage()
-  elif user == bot.user:
-    await ctx.send("Who, me?")
-    await usage()
-  else:
-    role = discord.utils.get(user.guild.roles, name="muted")
-    print(role)
-    if len(args) == 2:
-      await user.add_roles(role, reason=args[1])
-    else:
-      await user.add_roles(role)
+  if not is_mod(ctx.author):
     if not quiet_mode:
-      await ctx.send("Server muted " + user.display_name + "!")
+      await ctx.send("You don't have permission to do that!")
+    return
+
+  user = await get_user(ctx, args[0], "Usage: '.m[ute] @user [reason]")
+  if user != None:
+    role = discord.utils.get(user.guild.roles, name=mute_role)
+    if len(args) == 2:
+      if role in user.roles:
+        await user.remove_roles(role, reason=args[1]) # unmute
+        if not quiet_mode:
+          await ctx.send("Server unmuted " + user.display_name + "! Reason: " + args[1] + ".")
+      else:
+        await user.add_roles(role, reason=args[1]) # mute
+        if not quiet_mode:
+          await ctx.send("Server muted " + user.display_name + "! Reason: " + args[1] + ".")
+    else:
+      if role in user.roles:
+        await user.remove_roles(role) # unmute
+        if not quiet_mode:
+          await ctx.send("Server unmuted " + user.display_name + "!")
+      else:
+        await user.add_roles(role) # mute
+        if not quiet_mode:
+          await ctx.send("Server muted " + user.display_name + "!")
 
 
 # runs when message is received
